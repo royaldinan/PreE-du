82ee813d7ef0ec835392d24e3ee366d81051b9e0
royaldinan <ferdinandmanroe12@gmail.com>
2026-06-24 11:32:01 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['framer-motion', 'motion-dom'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/framer-motion/, /motion-dom/, /node_modules/],
+    },
+  },
 })
